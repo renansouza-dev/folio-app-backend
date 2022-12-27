a5928506682d0912ab248b6cfbb110a922b733f3
Renan Souza <renan@duck.com>
2022-12-27 18:51:58 -0300

    [#1] Add the transaction repository.

---
+++ src/main/java/com/renansouza/folio/transaction/TransactionRepository.java
@@ -0,0 +1,21 @@
+package com.renansouza.folio.transaction;
+
+import com.renansouza.folio.user.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Long> {
+
+    void deleteById(long id);
+
+    Transaction save(Transaction transaction);
+
+    List<Transaction> findAll();
+
+    Optional<Transaction> findById(long id);
+
+    Optional<User> findByClassificationIgnoreCase(String classification);
+
+}
